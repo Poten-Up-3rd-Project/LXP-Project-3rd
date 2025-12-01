f7a87b46a683399ea7886c41f72103b5806016b1
minyong96 <aldud159357@gmail.com>
2025-12-01 17:32:29 +0900

    feat: setup module

---
+++ recommand/src/test/java/com/lxp/recommand/RecommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.recommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tag/src/test/java/com/lxp/tag/TagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.tag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

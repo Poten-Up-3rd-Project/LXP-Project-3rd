31e1469f69ea62bdb462b0e9b03d260c711acb20
EomKun <zzang6535@gmail.com>
2025-12-01 16:09:47 +0900

    first commit

---
+++ Recommand/src/main/java/com/ogirafferes/recommand/RecommandApplication.java
@@ -0,0 +1,13 @@
+package com.ogirafferes.recommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommandApplication.class, args);
+    }
+
+}
---
+++ Tag/src/test/java/com/ogirafferes/tag/TagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogirafferes.tag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

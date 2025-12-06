e3678551969ce7e4955324a5d060d113d708ea95
youable <youable.framios@gmail.com>
2025-12-06 17:15:42 +0900

    refactor(tag): delete not use error code

--- tag/src/main/java/com/lxp/tag/domain/exception/TagErrorCode.java
+++ tag/src/main/java/com/lxp/tag/domain/exception/TagErrorCode.java
@@ -4,8 +4,6 @@ import com.lxp.common.domain.exception.ErrorCode;
 
 public enum TagErrorCode implements ErrorCode {
     INVALID_CHANGE_CATEGORY("TAG_001", "Invalid tag can not change category", "BAD_REQUEST"),
-    INVALID_ASSIGN_TAG("TAG_002", "Inactive category cannot be assigned tag", "BAD_REQUEST"),
-    FAIL_CREATE_TAG_IN_INVALID_CATEGORY("TAG_003", "Cannot create tag in invalid category", "BAD_REQUEST"),
     ;
 
     private final String code;

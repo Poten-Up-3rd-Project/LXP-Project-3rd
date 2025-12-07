a03e10dedd622c2f3f71b1588c2a0f961bfca743
minyong96 <aldud159357@gmail.com>
2025-12-08 01:02:11 +0900

    feat(content): create jpa entity
    - add mapper
    - add repository

--- content/src/main/java/com/lxp/content/ContentConfiguration.java
+++ content/src/main/java/com/lxp/content/ContentConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.modulith.ApplicationModule;
 
 @Configuration
 @ApplicationModule
-// TODO(이 부분 추후 라이브러리에서 제거 후 application 에서 config 설정 )
-@Import({JpaAuditingConfig.class})
+@Import(JpaAuditingConfig.class)
 public class ContentConfiguration {
 }

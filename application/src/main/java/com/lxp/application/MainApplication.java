89e11ecec3d080ab4b85414abc972658a68c9b22
minyong96 <aldud159357@gmail.com>
2025-12-08 01:02:11 +0900

    feat(content): create jpa entity
    - add mapper
    - add repository

--- application/src/main/java/com/lxp/application/MainApplication.java
+++ application/src/main/java/com/lxp/application/MainApplication.java
@@ -2,9 +2,13 @@ package com.lxp.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.lxp")
+@EnableJpaRepositories(basePackages = "com.lxp")
 public class MainApplication {
 
     public static void main(String[] args) {
--- content/src/main/java/com/lxp/content/course/application/service/CourseCreateService.java
+++ content/src/main/java/com/lxp/content/course/application/service/CourseCreateService.java
@@ -2,10 +2,15 @@ package com.lxp.content.course.application.service;
 
 import com.lxp.content.course.application.port.provided.dto.command.CourseCreateCommand;
 import com.lxp.content.course.application.port.provided.usecase.CourseCreateUseCase;
+import com.lxp.content.course.domain.repository.CourseRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CourseCreateService implements CourseCreateUseCase {
+    private CourseRepository courseRepository;
+
     @Override
     public Long execute(CourseCreateCommand input) {
         return 0L;

a54e66affe8f8e304d3025307f9416bdbd3dd46c
minyong96 <aldud159357@gmail.com>
2025-12-09 11:37:06 +0900

    feat: apply event handler

--- content/src/main/java/com/lxp/content/course/application/service/CourseCreateService.java
+++ content/src/main/java/com/lxp/content/course/application/service/CourseCreateService.java
@@ -1,18 +1,32 @@
 package com.lxp.content.course.application.service;
 
+import com.lxp.content.course.application.mapper.CourseResultMapper;
 import com.lxp.content.course.application.port.provided.dto.command.CourseCreateCommand;
+import com.lxp.content.course.application.port.provided.dto.result.CourseInfoResult;
 import com.lxp.content.course.application.port.provided.usecase.CourseCreateUseCase;
+import com.lxp.content.course.domain.model.Course;
 import com.lxp.content.course.domain.repository.CourseRepository;
+import com.lxp.content.course.domain.service.CourseCreateDomainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class CourseCreateService implements CourseCreateUseCase {
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
+    private final CourseCreateDomainService courseCreateDomainService;
+    private final CourseResultMapper resultMapper;
+    // TODO(userPort 포트주입)
 
     @Override
-    public Long execute(CourseCreateCommand input) {
-        return 0L;
+    public CourseInfoResult handle(CourseCreateCommand command) {
+        // user 가져오기
+        Course course = courseRepository.save(
+                courseCreateDomainService.create(command)
+        );
+
+        return resultMapper.toResult(course);
     }
 }

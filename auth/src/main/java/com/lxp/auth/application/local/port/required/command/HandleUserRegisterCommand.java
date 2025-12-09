403687da71a4b25a24d788ce3954c8aca9b2d816
dnya0 <annay01794@gmail.com>
2025-12-09 09:12:18 +0900

    feat: add learner profile fields to UserSaveCommand

--- auth/src/main/java/com/lxp/auth/application/local/port/required/command/HandleUserRegisterCommand.java
+++ auth/src/main/java/com/lxp/auth/application/local/port/required/command/HandleUserRegisterCommand.java
@@ -1,6 +1,16 @@
 package com.lxp.auth.application.local.port.required.command;
 
 import java.util.List;
+import java.util.UUID;
 
-public record HandleUserRegisterCommand(String email, String password, String name, String role, List<String> tags) {
+public record HandleUserRegisterCommand(
+    UUID userId,
+    String email,
+    String password,
+    String name,
+    String role,
+    List<Long> tags,
+    String learnerLevel,
+    String job
+) {
 }
--- user/src/main/java/com/lxp/user/application/user/command/UserSaveCommand.java
+++ user/src/main/java/com/lxp/user/application/user/command/UserSaveCommand.java
@@ -1,6 +1,16 @@
 package com.lxp.user.application.user.command;
 
+import java.util.List;
 import java.util.UUID;
 
-public record UserSaveCommand(UUID userId, String name, String email, String role) {
+public record UserSaveCommand(
+    UUID userId,
+    String name,
+    String email,
+    String role,
+
+    List<Long> Tags,
+    String learnerLevel,
+    String job
+) {
 }

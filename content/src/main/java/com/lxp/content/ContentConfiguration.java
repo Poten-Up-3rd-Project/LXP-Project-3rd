c2901019db702a9fb10a67f0db53225b73bf86c4
minyong96 <aldud159357@gmail.com>
2025-12-09 17:00:49 +0900

    chore: fix module

--- content/src/main/java/com/lxp/content/ContentConfiguration.java
+++ content/src/main/java/com/lxp/content/ContentConfiguration.java
@@ -1,9 +1,13 @@
 package com.lxp.content;
 
+import com.lxp.common.infrastructure.persistence.JpaAuditingConfig;
+import com.lxp.common.infrastructure.retry.DefaultRetryPolicy;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.modulith.ApplicationModule;
 
 @Configuration
 @ApplicationModule
+@Import({DefaultRetryPolicy.class, JpaAuditingConfig.class})
 public class ContentConfiguration {
 }

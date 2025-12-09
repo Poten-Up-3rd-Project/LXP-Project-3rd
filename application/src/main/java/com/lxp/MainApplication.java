c476fc81ae14c7f8e338d74843dd8624c6f6eafb
youable <youable.framios@gmail.com>
2025-12-09 15:34:59 +0900

    hotfix: modulith error fix

--- application/src/main/java/com/lxp/MainApplication.java
+++ application/src/main/java/com/lxp/MainApplication.java
@@ -1,8 +1,7 @@
-package com.lxp.application;
+package com.lxp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class MainApplication {
---
+++ application/src/main/java/com/lxp/application/RetryConfig.java
@@ -0,0 +1,29 @@
+package com.lxp.application;
+
+import com.lxp.common.retry.RetryPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RetryConfig {
+
+    @Bean
+    public RetryPolicy retryPolicy() {
+        return new RetryPolicy() {
+            @Override
+            public int getMaxAttempts() {
+                return 1;
+            }
+
+            @Override
+            public long getBackoffMillis(int attempt) {
+                return 0L;
+            }
+
+            @Override
+            public boolean shouldRetry(Exception e) {
+                return false;
+            }
+        };
+    }
+}

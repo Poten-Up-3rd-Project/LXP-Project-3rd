84008f1b4cbcc54ce27a6a84b3d9d6e2a9ab0987
Seohyun Kim <102886338+betterb2etr@users.noreply.github.com>
2025-12-11 00:54:57 +0900

    docs: readme.md preparing for 1211

--- recommend/src/test/java/com/lxp/recommend/RecommendApplicationTests.java
+++ recommend/src/test/java/com/lxp/recommend/RecommendApplicationTests.java
@@ -3,10 +3,11 @@ package com.lxp.recommend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class RecommendApplicationTests {
 
     @Test
     void contextLoads() {
+        // 스프링 컨텍스트가 정상적으로 로드되는지만 확인
     }
-
 }

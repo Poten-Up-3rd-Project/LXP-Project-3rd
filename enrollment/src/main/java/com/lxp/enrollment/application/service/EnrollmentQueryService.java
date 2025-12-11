bd726c08c6ac63ec7177c94a4e6292401ddd29be
youable <youable.framios@gmail.com>
2025-12-11 11:58:43 +0900

    Revert "feat(enrollment): enrollment list api"
    This reverts commit 21b125572f038ee8257795435bafe393dbf63614.

--- enrollment/src/main/java/com/lxp/enrollment/application/service/EnrollmentQueryService.java
+++ enrollment/src/main/java/com/lxp/enrollment/application/service/EnrollmentQueryService.java
@@ -1,6 +1,8 @@
 package com.lxp.enrollment.application.service;
 
 import com.lxp.common.domain.pagination.Page;
+import com.lxp.content.course.application.port.provided.dto.result.CourseResult;
+import com.lxp.content.course.application.port.provided.external.ExternalCourseSummaryPort;
 import com.lxp.enrollment.application.port.query.EnrollmentResult;
 import com.lxp.enrollment.application.port.query.EnrollmentSummaryResult;
 import com.lxp.enrollment.application.port.required.EnrollmentQueryPort;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EnrollmentQueryService {
     private final EnrollmentQueryPort enrollmentQueryPort;
+    private final ExternalCourseSummaryPort courseSummaryPort;
 
     public Page<EnrollmentSummaryResult> findEnrollments(String userId, String state, int page, int size) {
         Page<EnrollmentResult> enrollmentPage = enrollmentQueryPort.findEnrollments(userId, state, page, size);
@@ -21,10 +24,10 @@ public class EnrollmentQueryService {
         List<EnrollmentSummaryResult> summaryResults = new ArrayList<>();
         for (EnrollmentResult result : enrollmentPage.content()) {
             String courseUUID = result.courseUUID();
-//            CourseResult courseResult = courseSummaryPort.getCourseSummary(courseUUID)
-//                    .orElseThrow(() -> new IllegalStateException("course 없음"));
-//            EnrollmentSummaryResult summaryResult = EnrollmentSummaryResult.of(result, courseResult);
-//            summaryResults.add(summaryResult);
+            CourseResult courseResult = courseSummaryPort.getCourseSummary(courseUUID)
+                    .orElseThrow(() -> new IllegalStateException("course 없음"));
+            EnrollmentSummaryResult summaryResult = EnrollmentSummaryResult.of(result, courseResult);
+            summaryResults.add(summaryResult);
         }
 
         return Page.of(summaryResults, enrollmentPage.pageNumber(), enrollmentPage.pageSize(), enrollmentPage.totalElements());
--- enrollment/src/main/java/com/lxp/enrollment/interfaces/EnrollmentQueryController.java
+++ enrollment/src/main/java/com/lxp/enrollment/interfaces/EnrollmentQueryController.java
@@ -1,15 +1,7 @@
 package com.lxp.enrollment.interfaces;
 
-import com.lxp.enrollment.interfaces.dto.request.EnrollmentListRequest;
-import com.lxp.enrollment.interfaces.dto.response.EnrollmentPageResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
-
+@Controller
 public class EnrollmentQueryController {
-
-    public ResponseEntity<EnrollmentPageResponse> getEnrollments(EnrollmentListRequest request) {
-        return null;
-    }
 }

522c1d7b6d0f21a3c926fdff71aceb878c456230
dnya0 <annay01794@gmail.com>
2025-12-12 17:54:50 +0900

    feat: implement custom error handling for authentication and access denial

--- auth/src/main/java/com/lxp/auth/infrastructure/security/handler/CustomAuthenticationEntryPoint.java
+++ auth/src/main/java/com/lxp/auth/infrastructure/security/handler/CustomAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -17,6 +18,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write("{\"status\": 401, \"message\": \"Authentication failed. Please check your token.\"}");
+        request.setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, authException);
+        request.getRequestDispatcher("/api-v1/error/authentication-failed").forward(request, response);
     }
 }

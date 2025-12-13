753b4e74a588faf9f9924813002fd5b535377e30
dnya0 <annay01794@gmail.com>
2025-12-13 20:56:37 +0900

    feat: update user and auth endpoints to include versioning and modify request/response schemas

--- user/src/main/java/com/lxp/user/presentation/rest/UserController.java
+++ user/src/main/java/com/lxp/user/presentation/rest/UserController.java
@@ -15,6 +15,7 @@ import com.lxp.user.application.port.provided.usecase.WithdrawUserUseCase;
 import com.lxp.user.application.port.required.dto.AuthTokenResult;
 import com.lxp.user.presentation.rest.dto.request.UserUpdateRequest;
 import com.lxp.user.presentation.rest.dto.response.UserProfileResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -47,21 +48,27 @@ public class UserController {
     private final WithdrawUserUseCase withdrawUserUseCase;
 
     @GetMapping
-    public ApiResponse<UserProfileResponse> getUserInfo(@CurrentUserId String userId) {
+    public ApiResponse<UserProfileResponse> getUserInfo(
+        @Parameter(hidden = true)
+        @CurrentUserId String userId) {
         UserInfoDto userInfoDto = searchUserProfileUseCase.execute(new ExecuteSearchUserCommand(userId));
         return ApiResponse.success(UserProfileResponse.to(userInfoDto));
     }
 
     @PatchMapping
-    public ApiResponse<UserProfileResponse> updateUserInfo(@CurrentUserId String userId,
+    public ApiResponse<UserProfileResponse> updateUserInfo(@Parameter(hidden = true) @CurrentUserId String userId,
                                                            @RequestBody UserUpdateRequest request) {
-        UserInfoDto userInfoDto = updateUserProfileUseCase.execute(new ExecuteUpdateUserCommand(userId, request.name(), request.level(), request.tagIds(), request.job()));
+        UserInfoDto userInfoDto = updateUserProfileUseCase.execute(
+            new ExecuteUpdateUserCommand(userId, request.name(), request.level(), request.tagIds(), request.job())
+        );
         return ApiResponse.success(UserProfileResponse.to(userInfoDto));
     }
 
     @PreAuthorize("hasAuthority('ROLE_LEARNER')")
     @PutMapping("/role")
-    public ApiResponse<Void> updateUserToInstructor(@CurrentUserId String userId, HttpServletRequest request, HttpServletResponse response) {
+    public ApiResponse<Void> updateUserToInstructor(@Parameter(hidden = true) @CurrentUserId String userId,
+                                                    HttpServletRequest request,
+                                                    HttpServletResponse response) {
         String token = getCookie(request);
         AuthTokenResult execute = updateUserRoleUseCase.execute(new UpdateUserRoleCommand(userId, token));
         ResponseCookie cookie = ResponseCookie.from(CookieConstants.ACCESS_TOKEN_NAME, execute.accessToken())
@@ -78,7 +85,9 @@ public class UserController {
     }
 
     @DeleteMapping
-    public ApiResponse<Void> deleteUserInfo(@CurrentUserId String userId, HttpServletRequest request, HttpServletResponse response) {
+    public ApiResponse<Void> deleteUserInfo(@Parameter(hidden = true) @CurrentUserId String userId,
+                                            HttpServletRequest request,
+                                            HttpServletResponse response) {
         String cookie = getCookie(request);
         withdrawUserUseCase.execute(new ExecuteWithdrawUserCommand(userId, cookie));
 
